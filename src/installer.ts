878ffa640c5a01327dbe62baa265f71f84f21366
Chad Wilson <29788154+chadlwilson@users.noreply.github.com>
2025-06-04 17:20:48 +0800

    Shallow clone gauge repo when installing from source; while bumping dependencies

--- src/installer.ts
+++ src/installer.ts
@@ -50,7 +50,7 @@ export class Installer {
 	private async installFromSource() {
 		let gaugeDir: string = join(process.env['RUNNER_TEMP'] || '',
 			'temp_' + Math.floor(Math.random() * 2000000000), 'gauge');
-		await exec('git', ['clone', this.gauge_repo_url, gaugeDir])
+		await exec('git', ['clone', '--depth=1', this.gauge_repo_url, gaugeDir, ])
 		process.chdir(gaugeDir);
 		await exec('go', ['run', join('build', 'make.go')])
 		let toolPath = await cacheDir(join(gaugeDir, 'bin', `${this.getPlatform()}_${this.getExecutableArchitecture()}`), 'gauge', 'master');
