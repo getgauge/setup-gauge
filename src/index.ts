10cf422cdc7fb0344a7e758b3afbe0b27ee2f44d
Chad Wilson <29788154+chadlwilson@users.noreply.github.com>
2026-07-31 13:09:11 +0800

    fix: ensure errors are handled and thrown
    Signed-off-by: Chad Wilson <29788154+chadlwilson@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import { getInput, setFailed } from '@actions/core';
 import { Installer } from './installer';
 
-try {
+async function run() {
     const version = getInput('gauge-version');
     const plugins = getInput('gauge-plugins');
     const token = getInput('github-token');
     let installer = new Installer(version, plugins, token);
-    installer.install();
-} catch (error) {
-    setFailed(error.message);
+    await installer.install();
 }
+
+run().catch(error => setFailed(error.message));
